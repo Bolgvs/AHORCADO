cc763531a8d1185ebcbabc161b74b3515017b863
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-02 09:53:06 +0200

    main: Add simple, main package w/window creation

---
+++ RTS-M4-Ahorcado/src/main/java/main/mainApp.java
@@ -0,0 +1,12 @@
+package main;
+
+import view.AhorcadoWindow;
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		AhorcadoWindow ahorcado = new AhorcadoWindow();
+		ahorcado.main(null);
+	}
+
+}
