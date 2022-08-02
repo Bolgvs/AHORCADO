e4c71de8f733ec82dad02c71edf2fac0647c8135
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-08-02 12:03:35 +0200

    commit ventana

--- RTS-M4-Ahorcado/src/view/AhorcadoWindow.java
+++ RTS-M4-Ahorcado/src/view/AhorcadoWindow.java
@@ -9,6 +9,7 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JEditorPane;
 
 
 public class AhorcadoWindow {
@@ -56,7 +57,7 @@ public class AhorcadoWindow {
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.WHITE);
+		panel_1.setBackground(Color.BLACK);
 		panel_1.setBounds(0, 207, 224, 48);
 		frame.getContentPane().add(panel_1);
 		
@@ -73,67 +74,71 @@ public class AhorcadoWindow {
 		frame.getContentPane().add(btnNewButton_28);
 		
 		JButton btnNewButton_1 = new JButton("A");
-		btnNewButton_1.setBounds(10, 308, 39, 23);
+		btnNewButton_1.setBounds(10, 308, 46, 23);
 		frame.getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton = new JButton("C");
-		btnNewButton.setBounds(86, 308, 39, 23);
+		btnNewButton.setBounds(104, 308, 46, 23);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_2 = new JButton("B");
-		btnNewButton_2.setBounds(48, 308, 39, 23);
+		btnNewButton_2.setBounds(58, 308, 46, 23);
 		frame.getContentPane().add(btnNewButton_2);
 		
 		JButton btnNewButton_10 = new JButton("K");
-		btnNewButton_10.setBounds(10, 354, 39, 23);
+		btnNewButton_10.setBounds(10, 354, 46, 23);
 		frame.getContentPane().add(btnNewButton_10);
 		
 		JButton btnNewButton_6 = new JButton("G");
-		btnNewButton_6.setBounds(48, 331, 39, 23);
+		btnNewButton_6.setBounds(58, 331, 46, 23);
 		frame.getContentPane().add(btnNewButton_6);
 		
 		JButton btnNewButton_9 = new JButton("J");
-		btnNewButton_9.setBounds(162, 331, 37, 23);
+		btnNewButton_9.setBounds(193, 331, 46, 23);
 		frame.getContentPane().add(btnNewButton_9);
 		
 		JButton btnNewButton_18 = new JButton("R");
-		btnNewButton_18.setBounds(124, 376, 39, 23);
+		btnNewButton_18.setBounds(147, 376, 46, 23);
 		frame.getContentPane().add(btnNewButton_18);
 		
 		JButton btnNewButton_13 = new JButton("N");
-		btnNewButton_13.setBounds(124, 354, 39, 23);
+		btnNewButton_13.setBounds(147, 354, 46, 23);
 		frame.getContentPane().add(btnNewButton_13);
 		
 		JButton btnNewButton_4 = new JButton("E");
-		btnNewButton_4.setBounds(162, 308, 39, 23);
+		btnNewButton_4.setBounds(193, 308, 46, 23);
 		frame.getContentPane().add(btnNewButton_4);
 		
 		JButton btnNewButton_7 = new JButton("H");
-		btnNewButton_7.setBounds(86, 331, 39, 23);
+		btnNewButton_7.setBounds(104, 331, 46, 23);
 		frame.getContentPane().add(btnNewButton_7);
 		
 		JButton btnNewButton_16 = new JButton("P");
-		btnNewButton_16.setBounds(48, 376, 39, 23);
+		btnNewButton_16.setBounds(58, 376, 46, 23);
 		frame.getContentPane().add(btnNewButton_16);
 		
 		JButton btnNewButton_12 = new JButton("M");
-		btnNewButton_12.setBounds(84, 354, 41, 23);
+		btnNewButton_12.setBounds(104, 354, 46, 23);
 		frame.getContentPane().add(btnNewButton_12);
 		
 		JButton btnNewButton_23 = new JButton("W");
-		btnNewButton_23.setBounds(124, 400, 43, 23);
+		btnNewButton_23.setBounds(147, 400, 46, 23);
 		frame.getContentPane().add(btnNewButton_23);
 		
 		JButton btnNewButton_19 = new JButton("S");
-		btnNewButton_19.setBounds(162, 376, 39, 23);
+		btnNewButton_19.setBounds(193, 376, 46, 23);
 		frame.getContentPane().add(btnNewButton_19);
 		
 		JButton btnNewButton_5 = new JButton("F");
-		btnNewButton_5.setBounds(10, 331, 39, 23);
+		btnNewButton_5.setBounds(10, 331, 46, 23);
 		frame.getContentPane().add(btnNewButton_5);
 		
 		JButton btnNewButton_11 = new JButton("L");
-		btnNewButton_11.setBounds(48, 354, 37, 23);
+		btnNewButton_11.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_11.setBounds(58, 354, 46, 23);
 		frame.getContentPane().add(btnNewButton_11);
 		
 		JButton btnNewButton_20 = new JButton("T");
@@ -141,15 +146,15 @@ public class AhorcadoWindow {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_20.setBounds(10, 400, 39, 23);
+		btnNewButton_20.setBounds(10, 400, 46, 23);
 		frame.getContentPane().add(btnNewButton_20);
 		
 		JButton btnNewButton_25 = new JButton("Y");
-		btnNewButton_25.setBounds(48, 423, 39, 23);
+		btnNewButton_25.setBounds(10, 423, 46, 23);
 		frame.getContentPane().add(btnNewButton_25);
 		
 		JButton btnNewButton_17 = new JButton("Q");
-		btnNewButton_17.setBounds(86, 376, 41, 23);
+		btnNewButton_17.setBounds(104, 376, 46, 23);
 		frame.getContentPane().add(btnNewButton_17);
 		
 		JButton btnNewButton_15 = new JButton("O");
@@ -157,32 +162,36 @@ public class AhorcadoWindow {
 		frame.getContentPane().add(btnNewButton_15);
 		
 		JButton btnNewButton_26 = new JButton("Z");
-		btnNewButton_26.setBounds(124, 423, 39, 23);
+		btnNewButton_26.setBounds(58, 423, 46, 23);
 		frame.getContentPane().add(btnNewButton_26);
 		
 		JButton btnNewButton_21 = new JButton("U");
-		btnNewButton_21.setBounds(48, 400, 39, 23);
+		btnNewButton_21.setBounds(58, 400, 46, 23);
 		frame.getContentPane().add(btnNewButton_21);
 		
 		JButton btnNewButton_22 = new JButton("V");
-		btnNewButton_22.setBounds(86, 400, 39, 23);
+		btnNewButton_22.setBounds(104, 400, 46, 23);
 		frame.getContentPane().add(btnNewButton_22);
 		
 		JButton btnNewButton_8 = new JButton("I");
-		btnNewButton_8.setBounds(124, 331, 39, 23);
+		btnNewButton_8.setBounds(147, 331, 46, 23);
 		frame.getContentPane().add(btnNewButton_8);
 		
 		JButton btnNewButton_24 = new JButton("X");
-		btnNewButton_24.setBounds(162, 400, 39, 23);
+		btnNewButton_24.setBounds(193, 400, 46, 23);
 		frame.getContentPane().add(btnNewButton_24);
 		
 		JButton btnNewButton_14 = new JButton("Ñ");
-		btnNewButton_14.setBounds(162, 354, 39, 23);
+		btnNewButton_14.setBounds(193, 354, 46, 23);
 		frame.getContentPane().add(btnNewButton_14);
 		
 		JButton btnNewButton_4_1 = new JButton("D");
-		btnNewButton_4_1.setBounds(124, 308, 39, 23);
+		btnNewButton_4_1.setBounds(147, 308, 46, 23);
 		frame.getContentPane().add(btnNewButton_4_1);
+		
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setBounds(0, 142, 224, 35);
+		frame.getContentPane().add(editorPane);
 		btnNewButton_28.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
