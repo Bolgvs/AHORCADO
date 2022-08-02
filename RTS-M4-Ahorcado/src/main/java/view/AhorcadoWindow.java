aa09e4df21e7948321759621dfba27d27c88c43b
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-08-02 09:56:49 +0200

    Principio pantalla principal

--- RTS-M4-Ahorcado/src/main/java/view/AhorcadoWindow.java
+++ RTS-M4-Ahorcado/src/main/java/view/AhorcadoWindow.java
@@ -7,11 +7,17 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
+import java.awt.Color;
+import java.awt.Canvas;
+import java.awt.ScrollPane;
+import javax.swing.JInternalFrame;
+import javax.swing.JSplitPane;
+import javax.swing.JToolBar;
+import java.awt.Panel;
 
 public class AhorcadoWindow {
 	private JFrame frame;
-	
-	private JButton b, b1;
 
 	/**
 	 * Launch the application.
@@ -41,27 +47,65 @@ public class AhorcadoWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBackground(new Color(240, 240, 240));
+		frame.setBounds(100, 100, 499, 531);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-  
-        // Creating a new buttons
-        b = new JButton("button1");
-        b1 = new JButton("button2");
-  
-        // Creating a panel to add buttons
-        JPanel p = new JPanel();
-  
-        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
-        
-        // Adding buttons and textfield to panel
-        // using add() method
-        p.add(b);
-        p.add(b1);
-  
-        
-  
-        // Adding panel to frame
-        frame.add(p);
+		JLabel lblNewLabel = new JLabel("Menu");
+		lblNewLabel.setBounds(10, 11, 46, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Teclado");
+		lblNewLabel_2.setBounds(10, 301, 46, 14);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 314, 224, 167);
+		frame.getContentPane().add(panel);
+		
+		JButton btnNewButton_1 = new JButton("A");
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("B");
+		panel.add(btnNewButton_2);
+		
+		JButton btnNewButton = new JButton("C");
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_3 = new JButton("D");
+		panel.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("E");
+		panel.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("F");
+		panel.add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("G");
+		panel.add(btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("H");
+		panel.add(btnNewButton_7);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(0, 23, 224, 111);
+		frame.getContentPane().add(panel_2);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 242, 224, 48);
+		frame.getContentPane().add(panel_1);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Palabra secreta");
+		lblNewLabel_2_1.setBounds(10, 225, 129, 14);
+		frame.getContentPane().add(lblNewLabel_2_1);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(0, 156, 224, 58);
+		frame.getContentPane().add(panel_3);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBounds(234, 11, 239, 470);
+		frame.getContentPane().add(panel_4);
 	}
 }
