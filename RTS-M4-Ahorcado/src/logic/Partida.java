8c1dcee0e0f98425ad3baf79a32ea91f638fcff5
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-02 11:17:34 +0200

    logic: Minor changes

--- RTS-M4-Ahorcado/src/logic/Partida.java
+++ RTS-M4-Ahorcado/src/logic/Partida.java
@@ -8,29 +8,35 @@ import java.util.Random;
 public class Partida {
 	private String[] arrayPalabras = {"uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez"};
 	private List<String> palabras = Arrays.asList(arrayPalabras);
-	private String palabra = "";
+	
+	private String palabra = new String();
+	
 	private int intentos = 10;
-	private String palabraOculta = "";
+	
+	private String palabraOculta = new String();
 	private List<Character> letras;
+	
 	private char[] palabraArray = palabra.toCharArray();
 	
 	
-	private void generarPalabra() {
+	private void escogerPalabra() {
+		// Generate random integer to select a word from the string array
 		Random rand = new Random();
 		palabra = palabras.get(rand.nextInt(palabras.size()));
 	}
 	
-	public boolean contieneLetra(String a) {
+	public boolean contieneLetra(char a) {
+		// Add the letter to the guessed letters array
+		letras.add(a);
 		
-		letras.add(a.charAt(0));
-		return palabra.contains(a);
+		// Check if the current random word contains the character the user inputted
+		return palabra.contains(Character.toString(a));
 	}
-	
 	
 	public void actualizarPalabraOculta(char a) {
 		
-		for(int i=0; i<palabraArray.length; i++) {
-			if(letras.contains(palabraArray[i]))
+		for (int i = 0; i < palabraArray.length; i++) {
+			if (letras.contains(palabraArray[i]))
 				palabraOculta += palabraArray[i] + " ";
 			else
 				palabraOculta += "_ ";
@@ -38,7 +44,7 @@ public class Partida {
 	}
 	
 	public void comprobarLetra(char a, Button b) {
-		if(!contieneLetra(a + "")) {
+		if (!contieneLetra(a)) {
 			intentos--;
 		}
 		
